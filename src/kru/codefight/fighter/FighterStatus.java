5e5155b27132882e7bfff2bb8ad3a54cf385f2ec
Kru <kruljac.marko@gmail.com>
2015-10-24 20:46:04 +0200

    removed casting attack info

--- src/kru/codefight/fighter/FighterStatus.java
+++ src/kru/codefight/fighter/FighterStatus.java
@@ -1,19 +1,15 @@
 package kru.codefight.fighter;
 
-import kru.codefight.fighter.attacks.AbstractAttack;
-
 public class FighterStatus {
 
   private int hitPoints;
   private boolean isAttacking;
-  private AbstractAttack incomingAttack;
   private Stance stance;
 
   public void loadStatus(Fighter fighter) {
     this.stance = fighter.getStance(); //this possibly is not a deep copy!
     this.hitPoints = fighter.getHitPoints();
     this.isAttacking = fighter.isAttacking();
-    this.incomingAttack = fighter.getCastingAttack();
   }
 
   public Stance getStance() {
